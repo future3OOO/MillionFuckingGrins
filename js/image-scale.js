f5ea62df5e44a85d6917e43c2073b453ad550d42
Ryvix <575452-rwr@users.noreply.gitlab.com>
2020-05-08 18:25:55 -0400

    Stop IDE from updating 3rd party copyright headers

--- js/image-scale.js
+++ js/image-scale.js
@@ -1,33 +1,11 @@
-/*
- * @package       mds
- * @copyright     (C) Copyright 2020 Ryan Rhode, All rights reserved.
- * @author        Ryan Rhode, ryan@milliondollarscript.com
- * @version       2020.05.08 17:42:17 EDT
- * @license       This program is free software; you can redistribute it and/or modify
- *        it under the terms of the GNU General Public License as published by
- *        the Free Software Foundation; either version 3 of the License, or
- *        (at your option) any later version.
- *
- *        This program is distributed in the hope that it will be useful,
- *        but WITHOUT ANY WARRANTY; without even the implied warranty of
- *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *        GNU General Public License for more details.
- *
- *        You should have received a copy of the GNU General Public License along
- *        with this program;  If not, see http://www.gnu.org/licenses/gpl-3.0.html.
- *
- *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- *        Million Dollar Script
- *        A pixel script for selling pixels on your website.
- *
- *        For instructions see README.txt
- *
- *        Visit our website for FAQs, documentation, a list team members,
- *        to post any bugs or feature requests, and a community forum:
- *        https://milliondollarscript.com/
- *
- */
+// ==========================================================================
+// Project:     Image Scale
+// Description: Scale images to fit or fill any target size via two simple properties: scale and align.
+// Copyright:   ©2012-2016 GestiXi
+// License:     Licensed under the MIT license (see LICENCE)
+// Version:     2.2
+// Author:      Nicolas BADIA
+// ==========================================================================
 
 // Uses CommonJS, AMD or browser globals to create a jQuery plugin.
 (function(factory) {
