d73975ac926afdc97f07eb0ebcb97934b7625341
Ryan <575452-rwr@users.noreply.gitlab.com>
2020-11-13 09:09:37 -0500

    Update copyright version

--- include/currency_functions.php
+++ include/currency_functions.php
@@ -1,9 +1,9 @@
 <?php
-/**
+/*
  * @package       mds
  * @copyright     (C) Copyright 2020 Ryan Rhode, All rights reserved.
  * @author        Ryan Rhode, ryan@milliondollarscript.com
- * @version       2020.05.08 17:42:17 EDT
+ * @version       2020.11.13 08:57:00 EST
  * @license       This program is free software; you can redistribute it and/or modify
  *        it under the terms of the GNU General Public License as published by
  *        the Free Software Foundation; either version 3 of the License, or
